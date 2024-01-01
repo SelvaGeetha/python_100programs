87b7383b2f631ba0fd5aaaee7eb78fbc03d1bf30
SelvaGeetha <92872009+SelvaGeetha@users.noreply.github.com>
2024-01-01 18:35:18 +0530

    Add files via upload

---
+++ print.py
@@ -0,0 +1,4 @@
+print("I pray the god to sucessfully finsh this 100 program without wasting of time and lazyness")
+
+#output:
+#I pray the god to sucessfully finsh this 100 program without wasting of time and lazyness
